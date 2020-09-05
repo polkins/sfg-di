08ea35751fe1b1633127e67974f640d96f331adf
polkins <453287der>
2020-09-05 23:59:00 +0300

    inject without spring

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+        System.out.println(controller.sayHello());
+
+    }
 
 }
