3d4d70a181d771ca8e69205fe1ddb7c3cd281670
polkins <453287der>
2020-09-07 23:07:22 +0300

    add profiles

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +13,9 @@ public class SfgDiApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
         System.out.println(controller.sayHello());
+
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        i18nController.sayHello();
 
         System.out.println("property");
         PropertyInjectedController propertyInjectedController = (PropertyInjectedController)ctx.getBean("propertyInjectedController");
--- src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("greetingService")
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
